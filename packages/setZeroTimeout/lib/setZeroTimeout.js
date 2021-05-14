b84f27ffa7729a13e10574f105c96c029d5f0b6f
yilimin <yilimin@1b1k.net>
2021-05-14 15:17:32 +0800

    fix: node window

--- packages/setZeroTimeout/lib/setZeroTimeout.js
+++ packages/setZeroTimeout/lib/setZeroTimeout.js
@@ -5,15 +5,18 @@ module.exports = setZeroTimeout;
 function setZeroTimeout() {
     var timeouts = [];
     var messageName = 'zero-timeout-message';
-    let window = window || global;
+    let glo = global;
+    if (window) {
+        glo = window;
+    }
     // 保持 setTimeout 的形态，只接受单个函数的参数，延迟始终为 0。
     function setZeroTimeout(fn) {
         timeouts.push(fn);
-        window.postMessage(messageName, '*');
+        glo.postMessage(messageName, '*');
     }
 
     function handleMessage(event) {
-        if (event.source == window && event.data == messageName) {
+        if (event.source == glo && event.data == messageName) {
             event.stopPropagation();
             if (timeouts.length > 0) {
                 var fn = timeouts.shift();
@@ -22,10 +25,12 @@ function setZeroTimeout() {
         }
     }
 
-    window.addEventListener('message', handleMessage, true);
+    glo.addEventListener('message', handleMessage, true);
 
-    // 把 API 添加到 window 对象上
-    window.setZeroTimeout = setZeroTimeout;
+    // 把 API 添加到 glo 对象上
+    glo.setZeroTimeout = setZeroTimeout;
 
     return setZeroTimeout;
 }
+
+console.log(setZeroTimeout);
